8b9c1a6dd36f7635d3966e74bc1ccbd6553d1f03
mat100payette <matpayette@hotmail.com>
2018-01-17 18:25:08 -0500

    Initial commit

---
+++ Emgu_Cam/frmMain.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Emgu_Cam
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnWebcam = new System.Windows.Forms.Button();
+            this.lblFPS = new System.Windows.Forms.Label();
+            this.pnlCapture = new System.Windows.Forms.Panel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnWebcam
+            // 
+            this.btnWebcam.Location = new System.Drawing.Point(581, 371);
+            this.btnWebcam.Name = "btnWebcam";
+            this.btnWebcam.Size = new System.Drawing.Size(130, 33);
+            this.btnWebcam.TabIndex = 1;
+            this.btnWebcam.Text = "Start Capture";
+            this.btnWebcam.UseVisualStyleBackColor = true;
+            this.btnWebcam.Click += new System.EventHandler(this.btnWebcam_Click);
+            // 
+            // lblFPS
+            // 
+            this.lblFPS.AutoSize = true;
+            this.lblFPS.Location = new System.Drawing.Point(12, 371);
+            this.lblFPS.Name = "lblFPS";
+            this.lblFPS.Size = new System.Drawing.Size(33, 13);
+            this.lblFPS.TabIndex = 3;
+            this.lblFPS.Text = "FPS: ";
+            // 
+            // pnlCapture
+            // 
+            this.pnlCapture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlCapture.Location = new System.Drawing.Point(12, 12);
+            this.pnlCapture.Name = "pnlCapture";
+            this.pnlCapture.Size = new System.Drawing.Size(699, 353);
+            this.pnlCapture.TabIndex = 4;
+            this.pnlCapture.Resize += new System.EventHandler(this.pnlCapture_Resize);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 407);
+            this.Controls.Add(this.pnlCapture);
+            this.Controls.Add(this.lblFPS);
+            this.Controls.Add(this.btnWebcam);
+            this.Name = "frmMain";
+            this.Text = "CamTracker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnWebcam;
+        private System.Windows.Forms.Label lblFPS;
+        private System.Windows.Forms.Panel pnlCapture;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Emgu_Cam/frmMain.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+using System.Drawing.Drawing2D;
+
+namespace Emgu_Cam
+{
+    public partial class frmMain : Form
+    {
+        private VideoCapture capture = null;
+        private ImageBox viewer = null;
+        private bool capturing = false;
+        private CascadeClassifier faceCascade = null;
+        private CascadeClassifier handCascade = null;
+        private Pen facePen = new Pen(Color.DarkSlateBlue, 2);
+        private Pen handPen = new Pen(Color.Firebrick, 2);
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            InitCapture();
+
+            faceCascade = new CascadeClassifier(Environment.CurrentDirectory + "/haarcascade_frontalface_default.xml");
+            handCascade = new CascadeClassifier(Environment.CurrentDirectory + "/haarcascade_hand.xml");
+        }
+
+        private void InitCapture()
+        {
+            int margin = 10;
+            viewer = new ImageBox();
+            viewer.Location = new Point(margin, margin);
+            viewer.Width = pnlCapture.Width;
+            viewer.Height = pnlCapture.Height;
+            pnlCapture.Controls.Add(viewer);
+            viewer.SendToBack();
+
+            capture = new VideoCapture();
+
+            Refresh();
+        }
+
+        private void CaptureCam()
+        {
+            if (viewer != null)
+            {
+                Mat matImg = capture.QueryFrame();
+
+                Image<Gray, byte> img = new Image<Gray, byte>(matImg.Bitmap);
+                Rectangle[] faces = faceCascade.DetectMultiScale(img);
+                Rectangle[] hands = handCascade.DetectMultiScale(img);
+
+                using (Graphics gr = Graphics.FromImage(matImg.Bitmap))
+                {
+                    gr.SmoothingMode = SmoothingMode.AntiAlias;
+
+                    foreach (Rectangle face in faces)
+                        gr.DrawRectangle(facePen, new Rectangle(face.X, face.Y, face.Width, face.Height));
+
+                    foreach (Rectangle hand in hands)
+                        gr.DrawRectangle(handPen, new Rectangle(hand.X, hand.Y, hand.Width, hand.Height));
+                }
+
+                viewer.Image = matImg;
+            }
+        }
+
+        private void btnWebcam_Click(object sender, EventArgs e)
+        {
+            if (capturing)
+            {
+                timer.Stop();
+                capture.Stop();
+                viewer.Image = null;
+                btnWebcam.Text = "Start Capture";
+            }
+            else
+            {
+                capture.Start();
+                timer.Start();
+                btnWebcam.Text = "Stop Capture";
+            }
+            capturing = !capturing;
+        }
+
+        private void pnlCapture_Resize(object sender, EventArgs e)
+        {
+            viewer.Width = pnlCapture.Width;
+            viewer.Height = pnlCapture.Height;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            CaptureCam();
+        }
+    }
+}
+
